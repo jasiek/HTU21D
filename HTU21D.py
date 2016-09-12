5e6611a581d6088b640a8af9629e2b079586fb74
Jan Szumiec <jan.szumiec@gmail.com>
2016-09-12 09:26:18 +0200

    Moar precision.

--- HTU21D.py
+++ HTU21D.py
@@ -21,7 +21,7 @@ class HTU21D:
     def read_humidity(self):
         self.reset()
         msb, lsb, crc = self.bus.read_i2c_block_data(I2C_ADDR, CMD_TRIG_HUMID_HM, 3)
-        return -6 + 125 * (msb * 256 + lsb) / 65536
+        return -6 + 125 * (msb * 256 + lsb) / 65536.0
 
     def reset(self):
         self.bus.write_byte(I2C_ADDR, CMD_RESET)
